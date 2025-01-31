99a659bc3584cf76a59d9c06491a9514c6527169
somaharini <somaharini24@gmail.com>
2025-01-31 08:05:29 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Pattern1 {
+    public static void printPattern(int n) {
+        int i, j;
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]) {
+        int n = 4;
+        printPattern(n);
+    }
+}
